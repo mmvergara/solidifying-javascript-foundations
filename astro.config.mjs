6011ddcd19919846c934a60e2220444c11bb3f19
mmvergara <mark.jesusmanabat@gmail.com>
2025-01-27 05:35:45 +0800

    feat: update configuration and dependencies for Vercel integration and add Google Analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,26 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
+import partytown from "@astrojs/partytown";
+import vercel from "@astrojs/vercel";
+
 // https://astro.build/config
 export default defineConfig({
-  site: "https://example.com",
+  site: "https://solidifying-javascript-foundations.vercel.app",
 
-  integrations: [mdx(), sitemap()],
+  integrations: [
+    mdx(),
+    sitemap(),
+    partytown({
+      config: {
+        forward: ["dataLayer.push"],
+      },
+    }),
+  ],
+  output: "server",
+  adapter: vercel({
+    webAnalytics: {
+      enabled: true,
+    },
+  }),
 });
